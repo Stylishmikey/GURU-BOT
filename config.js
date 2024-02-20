a9490d245892afe0a2b9c021c3a4174470c87371
Stylishmikey <159425855+Stylishmikey@users.noreply.github.com>
2024-02-20 19:28:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['254798440793', 'mike', true],
+  ['254798440793', 'shoes', true], 
   [''] 
 ] //Number of owners
 
